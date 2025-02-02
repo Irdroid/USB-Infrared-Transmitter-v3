2769d857d407cb4334e7e54b037d38129dadc72a
Hardware Group LTD <info@irdroid.com>
2025-02-02 17:45:01 +0200

    Updated device descriptors

--- src/config.h
+++ src/config.h
@@ -10,16 +10,16 @@
 #define PIN_SCL             P17       // I2C SCL
 #define PIN_PWM             P34       // PWM pin
 // USB device descriptor
-#define USB_VENDOR_ID       0x16C0    // VID (shared www.voti.nl)
-#define USB_PRODUCT_ID      0x27DD    // PID (shared CDC)
-#define USB_DEVICE_VERSION  0x0100    // v1.0 (BCD-format)
+#define USB_VENDOR_ID       0x4d8    // VID Irdroid
+#define USB_PRODUCT_ID      0xf58b    // PID Irdroid
+#define USB_DEVICE_VERSION  0x0300    // v1.0 (BCD-format)
 
 // USB configuration descriptor
 #define USB_MAX_POWER_mA    100       // max power in mA 
 
 // USB descriptor strings
-#define MANUFACTURER_STR    'w','a','g','i','m','i','n','a','t','o','r'
-#define PRODUCT_STR         'O','L','E','D','-','T','e','r','m','i','n','a','l'
-#define SERIAL_STR          'g','i','t','h','u','b','.','c','o','m','/', \
-                            'w','a','g','i','m','i','n','a','t','o','r'
+#define MANUFACTURER_STR    'H','a','r','d','w','a','r','e',' ','G','r','o','u','p',' ','L','T','D'
+#define PRODUCT_STR         'U','S','B',' ','I','n','f','r','a','r','e','d',' ','T','r','a','n','s' \
+                            ,'c','e','i','v','e','r'
+#define SERIAL_STR          '3','0','0'
 #define INTERFACE_STR       'C','D','C',' ','S','e','r','i','a','l'
