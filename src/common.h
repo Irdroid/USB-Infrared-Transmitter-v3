5a955816be8a334da6ee6b6e71e78dd5109259fa
Hardware Group LTD <info@irdroid.com>
2025-02-03 11:11:19 +0200

    Description about the available debugging options

--- src/common.h
+++ src/common.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "config.h"
-
+/** Simple debugging via a connected OLED LCD Screen.
+ * There is no information about any other possible option (maybe uart).
+ * For now the debugging via the i2c connected OLED screen does the job.
+ */
 #ifdef DEBUG
 #include "stdio.h"
 __xdata uint8_t buffer[20];
